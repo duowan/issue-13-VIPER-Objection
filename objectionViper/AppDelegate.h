2702f298e39fb624813d4d65fa25bdff15e20047
PonyCui <cuiminghui@yy.com>
2015-01-20 10:50:02 +0800

    Initial Commit

---
+++ objectionViper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objectionViper
+//
+//  Created by 崔 明辉 on 15/1/20.
+//  Copyright (c) 2015年 多玩事业部 iOS开发组 YY Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objectionViperTests/objectionViperTests.m
@@ -0,0 +1,40 @@
+//
+//  objectionViperTests.m
+//  objectionViperTests
+//
+//  Created by 崔 明辉 on 15/1/20.
+//  Copyright (c) 2015年 多玩事业部 iOS开发组 YY Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface objectionViperTests : XCTestCase
+
+@end
+
+@implementation objectionViperTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
